d2bffa11002f5c43f5bd37df39fa79b8ee980743
niccolotubini <niccolo.tubini@studenti.unitn.it>
2017-09-29 11:20:23 +0200

    Rate filter

---
+++ src/main/java/filters/RateFilter.java
@@ -0,0 +1,30 @@
+package filters;
+
+public class RateFilter implements Filter{
+	
+	/** */
+	double minRate;
+	
+	/** */
+	double maxRate;
+	
+	
+	
+	/**
+	 * Instantiates a new RateFilter.
+	 *
+	 * @param minRate
+	 * @param maxRate
+	 */
+	public RateFilter(double minRate, double maxRate) {
+		System.out.println("Filter selected: RateFilter");
+		this.minRate = minRate;
+		this.maxRate = maxRate;
+	}
+	
+	public void Print(){
+		System.out.println("Filter's parameters are: \n"
+				+ "	min_rate: "  + minRate  + "\n"
+				+ "	max_rate: "  + maxRate  + "\n");
+	}
+}
