bc6a7eb7e65589cff0f4c45c26d6420e76fa092a
niccolotubini <niccolo.tubini@studenti.unitn.it>
2017-09-29 11:19:05 +0200

    Max filter

---
+++ src/main/java/filters/MaxFilter.java
@@ -0,0 +1,36 @@
+package filters;
+
+public class MaxFilter implements Filter{
+	
+	/** The high threshold*/
+	double maxVal;
+	
+	/** */
+	double maxSoft;
+	
+	/** */
+	boolean isSoft;
+	
+	
+	
+	/**
+	 * Instantiates a new MaxFilter.
+	 *
+	 * @param maxVal is the high threshold
+	 * @param maxSoft
+	 * @param isSoft
+	 */
+	public MaxFilter(double maxVal, double maxSoft, boolean isSoft) {
+		System.out.println("Filter selected: MinFilter");
+		this.maxVal = maxVal;
+		this.maxSoft = maxSoft;
+		this.isSoft = isSoft;
+	}
+	
+	public void Print(){
+		System.out.println("Filter's parameters are: \n"
+				+ "	max_val:  " + maxVal  + "\n"
+				+ "	max_soft: " + maxSoft + "\n"
+				+ "	is_soft:  " + isSoft  + "\n");
+	}
+}
